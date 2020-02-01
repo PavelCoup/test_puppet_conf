31257456b3f2375155ed4cb984f087b164995412
PavelCoup <pavel.coup@gmail.com>
2020-02-01 19:26:51 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -18,6 +18,15 @@ node 'slave2.puppet' {
    
    class { 'php': }
    
+   apache :: fastcgi :: server  {  'php' : 
+      host        =>  '127.0.0.1:9000' , 
+      timeout     =>  15 , 
+      flush       =>  false , 
+      faux_path   =>  '/var/www/php.fcgi' , 
+      fcgi_alias  =>  '/php.fcgi' , 
+      file_type   =>  'application / x-httpd-php' 
+      }
+   
    class { 'apache': }
    
    file { '/root/README':
