b23f2fad27721b9b57763550b53f1c3eb5a3ba78
PavelCoup <pavel.coup@gmail.com>
2020-02-01 20:50:56 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -17,7 +17,7 @@ node 'slave1.puppet' {
 node 'slave2.puppet' {
    
    class { 'apache': }
-   class { 'apache::fastcgi::server': }
+
    apache::fastcgi::server  {  'php' : 
       host        =>  '127.0.0.1:9000' , 
       timeout     =>  15 , 
