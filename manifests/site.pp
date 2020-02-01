d2e61ac241929f5a4a3f67fb5ba96707d40a814e
PavelCoup <pavel.coup@gmail.com>
2020-02-01 18:15:19 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -16,16 +16,7 @@ node 'slave1.puppet' {
 
 node 'slave2.puppet' {
    
-   class { '::php':
-      settings   => {
-         'PHP/max_execution_time'  => '90',
-         'PHP/max_input_time'      => '300',
-         'PHP/memory_limit'        => '64M',
-         'PHP/post_max_size'       => '32M',
-         'PHP/upload_max_filesize' => '32M',
-         'Date/date.timezone'      => 'Europe/Minsk',
-         },
-      }
+   class { 'php': }
    
    class { 'apache': }
    
