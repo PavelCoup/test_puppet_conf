441a8ea6bea34d1206479f7d273e836e638a05a8
PavelCoup <pavel.coup@gmail.com>
2020-02-03 02:32:53 +0300

    Delete stop.sh

--- modules/mineserver/files/stop.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-/opt/minecraft/mcrcon -H localhost -P 2560 -p password say Server is shutting down in 15 seconds
-/opt/minecraft/mcrcon -H localhost -P 2560 -p password save-all
-sleep 15s
-/opt/minecraft/mcrcon -H localhost -P 2560 -p password stop
-sleep 10s
