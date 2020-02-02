a7869ca3cf56233c5cad246b624a990fbb128f9f
PavelCoup <pavel.coup@gmail.com>
2020-02-02 14:45:18 +0300

    Update init.pp

--- modules/mineserver/manifests/init.pp
+++ modules/mineserver/manifests/init.pp
@@ -3,5 +3,6 @@ sudo mkdir /opt/minecraft
 cd /opt/minecraft
 sudo curl -O https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
 sudo java -Xmx1024M -Xms1024M -jar server.jar nogui
-sed -i 's/false/true/g' /opt/minecraft/eula.txt
+sudo chmod -R 777 /opt/minecraft
+sudo sed -i 's/false/true/g' /opt/minecraft/eula.txt
 sudo java -Xmx1024M -Xms1024M -jar server.jar nogui
