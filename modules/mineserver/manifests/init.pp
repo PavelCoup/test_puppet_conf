26db7304f4bc45b6340fc2265c90c52e13e50ad0
PavelCoup <pavel.coup@gmail.com>
2020-02-02 16:03:24 +0300

    Update init.pp

--- modules/mineserver/manifests/init.pp
+++ modules/mineserver/manifests/init.pp
@@ -21,6 +21,8 @@ class mineserver {
     }
   
   file { '/opt/minecraft/server.jar':
+    owner => 'root',
+    group => 'root',
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
     replace => false,
