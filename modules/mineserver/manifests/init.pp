4b549bd0d7ca68388229535fdcf1edf8ead63455
PavelCoup <pavel.coup@gmail.com>
2020-02-02 14:43:37 +0300

    Update init.pp

--- modules/mineserver/manifests/init.pp
+++ modules/mineserver/manifests/init.pp
@@ -2,5 +2,6 @@ sudo yum -y install java-1.8.0-openjdk-devel
 sudo mkdir /opt/minecraft
 cd /opt/minecraft
 sudo curl -O https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
+sudo java -Xmx1024M -Xms1024M -jar server.jar nogui
 sed -i 's/false/true/g' /opt/minecraft/eula.txt
 sudo java -Xmx1024M -Xms1024M -jar server.jar nogui
