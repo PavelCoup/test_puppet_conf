56d489fd9972e90d3ccc7324f4187a1ebdd101e7
PavelCoup <pavel.coup@gmail.com>
2020-02-03 22:23:21 +0300

    Update download_site.pp

--- site/profile/manifests/download_site.pp
+++ site/profile/manifests/download_site.pp
@@ -1,9 +1,10 @@
 class profile::download_site (
   String $url_site_download,
+  String $format_file,
 )
 
 {
-  file { '/var/www/html/index.html':
+  file { "/var/www/html/index.${format_file}":
       ensure => file,
       source => $url_site_download,
       replace => false,
