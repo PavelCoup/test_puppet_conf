9025d4e4dd22f3c4d404669427eb64d7f36860d7
PavelCoup <pavel.coup@gmail.com>
2020-02-03 22:23:48 +0300

    Update slave2.pp

--- site/role/manifests/slave2.pp
+++ site/role/manifests/slave2.pp
@@ -3,5 +3,6 @@ include profile::remove_readme
 include profile::web_servers::dynamic
 class { 'profile::download_site' : 
   url_site_download => 'https://raw.githubusercontent.com/PavelCoup/test_puppet_conf/production/files/index.php',
+  format_file => 'php'
   }
 }
