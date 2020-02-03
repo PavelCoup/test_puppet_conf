f1ad7d8b5578bd2014730f7323a2ebd6de2a0cf8
PavelCoup <pavel.coup@gmail.com>
2020-02-03 22:27:22 +0300

    Update slave1.pp

--- site/role/manifests/slave1.pp
+++ site/role/manifests/slave1.pp
@@ -1,7 +1,8 @@
-class role::slave1 (
-$url_site_download = 'https://raw.githubusercontent.com/PavelCoup/test_puppet_conf/production/files/index.html',
-) {
+class role::slave2 {
 include profile::remove_readme
-include profile::download_site
-include profile::web_servers::static
+include profile::web_servers::dynamic
+class { 'profile::download_site' : 
+  url_site_download => 'https://raw.githubusercontent.com/PavelCoup/test_puppet_conf/production/files/index.html',
+  format_file => 'html'
+  }
 }
