28c35e23f9a2d565d1968898d4cf63ca8eca82ff
PavelCoup <pavel.coup@gmail.com>
2020-02-03 20:22:15 +0300

    Update reverse_proxy.pp

--- profile/manifests/web_servers/reverse_proxy.pp
+++ profile/manifests/web_servers/reverse_proxy.pp
@@ -4,9 +4,9 @@ class profile::web_servers::reverse_proxy (
 )
 {
   include nginx
-  nginx::resource::server { '$ip_proxy:$port_proxy' :
+  nginx::resource::server { "192.168.1.10:80" :
      ensure      => present,
      listen_port => 80,
-     proxy       => 'http://192.168.1.12:$port_proxy',
+     proxy       => "${ip_proxy}:${port_proxy}",
      }
 }
